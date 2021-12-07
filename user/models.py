4160e2f2f31cc2a12a893640839cebf2492b5aec
feddykip <ferdinand.kiplangat@student.moringaschool.com>
2021-12-07 18:50:19 +0300

    deployment

---
+++ user/models.py
@@ -0,0 +1,111 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django .utils import timezone
+import datetime as dt
+import cloudinary
+from cloudinary.models import CloudinaryField
+
+# Create your models here.
+class Image (models.Model):
+    image = CloudinaryField('images',blank = True)
+    image_name = models.CharField(max_length=55)
+    image_caption = models.CharField(max_length=55)
+    profile = models.ForeignKey(User,on_delete=models.CASCADE, null = True)
+    likes = models.PositiveIntegerField(default = 0)
+    date_posted = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.image_name
+    
+    def save_image(self):
+        self.save()
+
+    
+    @classmethod
+    def get_image_by_id(cls,id):
+        images = cls.objects.get(id=id)
+        return images
+
+    @classmethod
+    def get_single_photo(cls,id):
+        image = cls.objects.get(pk=id)
+        return image
+
+    @classmethod
+    def delete_image(cls,id):
+        cls.objects.filter(id).delete()
+
+    @classmethod
+    def display_images(cls):
+        image = cls.objects.all()
+        return image
+
+    @classmethod
+    def update_caption(cls,id,new_caption):
+        cls.objects.filter(id=id).update(image_caption=new_caption)
+
+    def likes(self):
+        pass
+        # return self.likes.count()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null= True)
+    name = models.CharField(blank = True,max_length = 30)
+    email = models.CharField(blank = True, max_length = 100)
+    bio = models.TextField(max_length=100)
+    profile_image = models.ImageField(upload_to = 'images/')
+    follow = models.ManyToManyField(User, related_name='follows',blank = True)
+    
+    def __str__(self):
+        return self.name
+
+    def save_profile(self):
+        self.save()
+        
+    @classmethod
+    def get_profile(cls,id):
+        profile = Profile.objects.all()
+        return profile
+        
+    @classmethod
+    def update_profile(cls,id,new_name):
+        cls.objects.filter(id=id).update(name = new_name)
+
+    @classmethod
+    def delete_profile(cls,id):
+        cls.objects.filter(id).delete()
+
+
+    @classmethod
+    def search_by_name(cls, searched_name):
+        username = cls.objects.filter(name__icontains=searched_name)
+
+        return username
+
+
+
+class Comments(models.Model):
+    comment = models.TextField(blank = True, max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank = True)
+    image = models.ForeignKey(Image, blank=True, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)     
+
+    def __str__(self):
+        return self.comment
+
+    def save_comment(self):
+        self.save()
+    
+    
+    @classmethod
+    def update_comment(cls,id,new_comment):
+        cls.objects.filter(id=id).update(comment = new_comment)
+        
+    @classmethod
+    def get_comments(cls,id):
+        comments = cls.objects.filter(image__id=id)
+        return comments
+    
+
+    @classmethod
+    def delete_comment(cls,id):
+        cls.objects.filter(id).delete()
---
+++ user/views.py
@@ -0,0 +1,162 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from . models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from .forms import ProfileForm,CommentsForm, ImageForm
+from django.contrib.auth import authenticate,login,logout
+
+# Create your views here.
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = UserCreationForm()
+        if request.method == 'POST':
+            form =UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                
+                return redirect('login')
+        context = {'form': form}
+        return render(request,'registration/registration_form.html',  context)
+        
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = UserCreationForm()
+        if request.method == 'POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user = authenticate(request, username=username ,password=password)
+            if user is not None:   
+                login(request, user)
+        context={'form': form}
+        return render(request,'registration/login.html',  context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+@login_required(login_url='/accounts/login/')
+def post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.Author = current_user
+            post.save()
+        return redirect('loginPage')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'post.html', {"form": form})        
+
+@login_required(login_url='login/')
+def indexPage(request):
+    images=Image.objects.all()
+    comments=Comments.objects.all()
+    profile = Profile.objects.all()
+    return render(request,'index.html',{"images":images,"comments":comments, "profile":profile})
+
+
+
+def profile(request):
+    current_user=request.user
+    profile_info = Profile.objects.filter(user=current_user).first()
+    posts =  request.user.image_set.all()
+    return render(request,'django_registration/profile.html',{"images":posts,"profile":profile_info,"current_user":current_user})
+
+def search_username(request):
+
+    if 'search_username' in request.GET and request.GET["search_username"]:
+        username=request.user
+        searched_name = request.GET.get("search_username")
+        searched_user = User.objects.filter(username__icontains=search_username)
+        message = f"{searched_name}"
+
+        return render(request, 'search.html', {"message": message, "username": username})
+
+    else:
+        message = "username not found"
+        return render(request, 'search.html', {"message": message})
+    
+def upload_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+        return redirect('/')
+
+    else:
+        form = ImageForm()
+        return render(request,'upload_image.html', {"form":form})
+
+
+
+
+
+def image_likes(request,id):
+    image =  Image.get_single_photo(id)
+    user = request.user
+    user_id = user.id
+    
+    if user.is_authenticated:
+    
+        image.save()
+        
+    return redirect('indexPage')
+
+def add_comment(request,id):
+    current_user = request.user
+    image = Image.get_single_photo(id=id)
+    if request.method == 'POST':
+        form = CommentsForm(request.POST)
+       
+        
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = current_user
+            comment.image_id = id
+            comment.save()
+        return redirect('/')
+
+    else:
+        form = CommentsForm()
+        return render(request,'add_comment.html',{"form":form,"image":image})  
+    
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+      
+        form = ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.editor = current_user
+            image.save()
+        return redirect ('profile')
+
+    else:
+        form = ProfileForm()
+        return render(request,'django_registration/edit_profile.html',{"form":form})
+    
+    
+
+
+def create_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.profile = current_user
+            image.save()
+        return redirect('/')
+
+    else:
+        form = ImageForm()
+    return render(request, 'new_post.html', {"form": form})    
